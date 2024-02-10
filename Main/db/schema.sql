8d182caef7b337c85198c65a34e0aa50c8aebe7f
Matthew_Hill <hillmatt58@gmail.com>
2024-02-10 12:54:53 -0700

    view employees by department function

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -1,20 +1,20 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;  
 
-USE employees;
+USE employee_db;
 
 CREATE TABLE department (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) UNIQUE NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) UNIQUE NOT NULL,
-  salary DECIMAL UNSIGNED NOT NULL,
+  title VARCHAR(60) NOT NULL,
+  salary DECIMAL NOT NULL,
   department_id INT UNSIGNED NOT NULL,
-  INDEX dep_ind (department_id),
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+  INDEX dpt_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -23,8 +23,8 @@ CREATE TABLE employee (
   last_name VARCHAR(30) NOT NULL,
   role_id INT UNSIGNED NOT NULL,
   INDEX role_ind (role_id),
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id),
   manager_id INT UNSIGNED,
   INDEX man_ind (manager_id),
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  CONSTRAINT fk_mgr FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
--- Main/db/seed.sql
+++ Main/db/seed.sql
@@ -1,33 +1,34 @@
-use employees;
+USE employee_db;
 
 INSERT INTO department
     (name)
 VALUES
     ('Sales'),
-    ('Engineering'),
-    ('Finance'),
-    ('Legal');
+    ('Security'),
+    ('Manufacturing'),
+    ('Supply Chain');
 
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('Sales Lead', 100000, 1),
-    ('Salesperson', 80000, 1),
-    ('Lead Engineer', 150000, 2),
-    ('Software Engineer', 120000, 2),
-    ('Account Manager', 160000, 3),
-    ('Accountant', 125000, 3),
-    ('Legal Team Lead', 250000, 4),
-    ('Lawyer', 190000, 4);
+    ('Manufacturing Manager', 100000, 3),
+    ('Manufacturing Assistant Manager', 70000, 3),
+    ('Sales Lead', 60000, 1),
+    ('Sales Associate', 60000, 1),
+    ('Supply Chain Manager', 80000, 4),
+    ('Supply Chain Associate', 40000, 4),
+    ('Manufacturing Operator', 50000, 3),
+    ('Security Officer', 50000, 2);
 
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
+    ('Marcus', 'Parks', 1, NULL),
+    ('Shelly', 'Bristol', 2, NULL),
+    ('Elena', 'Harper', 3, 1),
+    ('Nolan', 'Wright', 4, 2),
+    ('Sierra', 'Tate', 5, NULL),
+    ('Dominic', 'Reed', 6, 3),
+    ('Aria', 'Monroe', 7, 4),
+    ('Caleb', 'Foster', 8, 6);
+    
